6a0073670444802e69d8e754a0f82bba7e7ece43
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-15 15:59:45 +0300

    Update app.component.ts Load the AppComponent in app.module.ts file Add styling to the HTML Template

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+  <div class="well">
+    <h1>Bluebirds Meal Tracker</h1>
+</div>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
