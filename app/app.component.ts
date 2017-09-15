e845349a512e6012216a4d9fca04cf5d8dcf4214
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-15 16:15:13 +0300

    Create display-meal.components.ts Load DisplayMealComponent in app.module Connect display meals to AppComponent Add styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
   <div class="well">
     <h1>Bluebirds Meal Tracker</h1>
 </div>
+<div class="well">
+  <meals-list></meals-list>
+</div>
   </div>
   `
 })
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import {DisplayMealComponent} from './display-meal.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [ AppComponent ],
+  declarations:[
+     AppComponent,
+     DisplayMealComponent
+   ],
+
   bootstrap:    [ AppComponent ]
 })
 
