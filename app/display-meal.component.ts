0bc858f488528546b15b3d8c1e2badc93f62c950
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-21 16:41:08 +0300

    Ability to select menu from option menu

--- app/display-meal.component.ts
+++ app/display-meal.component.ts
@@ -1,25 +1,24 @@
-    import {Component, Input, Output, EventEmitter} from '@angular/core';
-    import { Meal } from './meal.model';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
 
-    @Component({
-      selector: 'meals-list',
-      template: `
+@Component({
+  selector: 'meals-list',
+  template: `
     <div class="well">
 
     <div class ="displayMeals">
     <h2>MEALS LOG DISPLAY</h2>
-      <div class="displayMealHeading">
+
         <select (change)="menuSelected($event.target.value)">
-          <option value="highCalories" >High Calorie Meals</option>
+          <option value="highCalories" selected = "selected">High Calorie Meals</option>
           <option value="lowCalories">Low Calories Meals</option>
-          <option value="allMeals" selected="selected">All Meals</option>
+          <option value="allMeals">All Meals</option>
         </select>
-     </div>
         </div>
         <div class="displayMeal" *ngFor="let meal of childMealList | caloriesCount:selectedCalories">
           <table>
         <tr>
-          <td>{{meal.mealName}}</td>
+          <td>{{meal.ealName}}</td>
           <td>{{meal.description}}</td>
           <td>{{meal.calories}}</td>
           <td><button class="editbtn" (click)="editSelectedMeal(meal)">Edit</button></td>
@@ -28,18 +27,17 @@
       </div>
         </div>
       `
-    })
+})
 
-    export class DisplayMealComponent{
-    @Input() childMealList: Meal[];
-    @Output() clickMealSender = new EventEmitter();
-    editSelectedMeal(clickedMeal: Meal){
-      this.clickMealSender.emit(clickedMeal);
-    }
-    public selectedCalories: string = "allMeals";
-    menuSelected(optionFromMenu) {
-      this.selectedCalories = optionFromMenu;
-      console.log(this.selectedCalories);
-    }
-
+export class DisplayMealComponent {
+  @Input() childMealList: Meal[];
+  @Output() clickMealSender = new EventEmitter();
+  public selectedCalories: string = "allMeals";
+  menuSelected(optionFromMenu) {
+    this.selectedCalories = optionFromMenu;
+    console.log(this.selectedCalories);
+  }
+  editSelectedMeal(clickedMeal: Meal) {
+    this.clickMealSender.emit(clickedMeal);
+  }
 }
